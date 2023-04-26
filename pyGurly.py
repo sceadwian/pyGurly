aa56acaf1b1e7509f59d312a9b2c0545221f9847
sceadwian <leo.silenieks+accounts@alumni.utoronto.ca>
2023-04-25 22:58:48 -0400

    split actions between player and mob

--- pyGurly.py
+++ pyGurly.py
@@ -58,13 +58,30 @@ actions = [] # list of actions available to the player
 def combat_loop(player_stats, enemy_stats):
     #initialize combat variables
     round_num = 1
-    player_hp = int(player_stats[5])
-    enemy_hp = int(enemy_stats[5])
+    player_hp = int(player_stats["HP"])
+    enemy_hp = int(enemy_stats["HP"])
     
-    #load actions from player's CSV file
-    with open(f"{player_stats[0]}.csv", "r") as f:
-        reader = csv.reader(f)
-        actions = list(reader)[1][11:16] # get actions from the row containing player stats - Act1,Act2,Act3,Act4,Act5
+    #load actions from player's CSV file -> player_stats[12:17] -> 
+    player_actions = []
+    for i in range(1, 6):
+        action_id = int(player_stats[f"Act{i}"])
+        with open("own_actions.csv", "r") as f:
+            reader = csv.reader(f)
+            for row in reader:
+                if int(row[0]) == action_id:
+                    player_actions.append(row[1])
+                    break
+    
+    #load actions from enemy's CSV file -> enemy_stats[12:17] -> 
+    enemy_actions = []
+    for i in range(1, 6):
+        action_id = int(enemy_stats[f"Act{i}"])
+        with open("mob_actions.csv", "r") as f:
+            reader = csv.reader(f)
+            for row in reader:
+                if int(row[0]) == action_id:
+                    enemy_actions.append(row[1])
+                    break
     
     #combat loop
     while player_hp > 0 and enemy_hp > 0:
@@ -73,21 +90,21 @@ def combat_loop(player_stats, enemy_stats):
         
         # display action options
         print("Choose an action:")
-        for i, action in enumerate(actions):
+        for i, action in enumerate(player_actions):
             print(f"{i+1}. {action}")
         # prompt player for input
         while True:
             choice = input("Enter the number of the action you want to use: ")
-            if choice.isdigit() and int(choice) in range(1, len(actions)+1):
+            if choice.isdigit() and int(choice) in range(1, len(player_actions)+1):
                 break
             else:
                 print("Invalid input. Please enter the number of the action you want to use.")
         # use the chosen action
-        player_action = actions[int(choice)-1]
+        player_action = player_actions[int(choice)-1]
         # calculate damage and defense
-        player_dmg = int(player_stats[7]) * random.uniform(0.8, 1.2) # stamina * random multiplier
-        enemy_def = int(enemy_stats[9]) * random.uniform(0.8, 1.2) # defense * random multiplier
-        enemy_dmg = int(enemy_stats[7]) * random.uniform(0.8, 1.2) - enemy_def # stamina * random multiplier - enemy defense
+        player_dmg = int(player_stats["Stamina"]) * random.uniform(0.8, 1.2) # stamina * random multiplier
+        enemy_def = int(enemy_stats["Defense"]) * random.uniform(0.8, 1.2) # defense * random multiplier
+        enemy_dmg = int(enemy_stats["Stamina"]) * random.uniform(0.8, 1.2) - enemy_def # stamina * random multiplier - enemy defense
         if enemy_dmg < 0:
             enemy_dmg = 0
         # update HP
@@ -101,20 +118,18 @@ def combat_loop(player_stats, enemy_stats):
     # determine winner and update stats
     if player_hp <= 0:
         print("You lost the battle.")
-        update_character_stats(player_stats[0], 0, None) # update XP and action
+        update_character_stats(player_stats["Name"], 0, None) # update XP and action
         return "Enemy"
     else:
         print("You won the battle!")
-        xp_gain = 50 # fixed XP gain for winning - XP should be one of the values pulled from the csv file need to add that.
-        #maybe i can use the same structure for mobs as the csv file for players, but instead of height and weight being there which
-        #I intend to use to calculate size. instead that's where the, xp and loot info comes in... 
-        # maybe petty mobs will drop crappy actions and bosses drop the good actions
+        xp_gain = int(enemy_stats["XP"]) # XP gain for winning
         new_action = None # placeholder for action to be gained
         if round_num == 4: # boss fight
             xp_gain += 100 # extra XP for beating the boss
             new_action = "Super Move" # new action gained for beating the boss
-        update_character_stats(player_stats[0], xp_gain, new_action) # update XP and action
+        update_character_stats(player_stats["Name"], xp_gain, new_action) # update XP and action
         return "Player"
+
 
 
 
